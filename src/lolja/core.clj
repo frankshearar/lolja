f29e0c0160ec896894247e41ccd5dce43e938503
Frank Shearar <frank.shearar@gmail.com>
2012-01-23 11:23:39 +0000

    Turtle creation, pen states.

--- src/lolja/core.clj
+++ src/lolja/core.clj
@@ -1,1 +1,40 @@
-(ns lolja.core)
+(ns ;^{:doc "Turtle graphics for the browser."
+    ;  :author "Frank Shearar"}
+  lolja.core)
+
+
+; Questions: do we want switchable coordinates? (cartesion or polar!)
+;(defstruct turtle
+;  :location
+;  :heading
+;  :pen-state)
+
+(defn read-command [str]
+  (let [toks (.split str " ")]
+    (cons
+     (keyword (first toks))
+     (map #(Integer/valueOf %) (rest toks)))))
+
+(defn new-turtle []
+  {:location '(0 0) :heading 0 :pen-state :pen-down})
+
+(defn fd [turtle n]
+  "Move the turtle forward n units."
+  turtle)
+
+(defn pen-erase [turtle]
+  "Erase any lines over which the turtle moves."
+  (assoc turtle :pen-state :pen-erase))
+
+(defn pen-down [turtle]
+  "Lower the turtle's pen, so that it may draw as it moves."
+  (assoc turtle :pen-state :pen-down))
+
+(defn pen-state [turtle]
+  "In what state is the turtle's pen? Should be one of:
+   :pen-down, :pen-up, :pen-erase"
+  (:pen-state turtle))
+
+(defn pen-up [turtle]
+  "Lift the turtle's pen up, making it not draw lines during movement."
+  (assoc turtle :pen-state :pen-up))
--- test/lolja/test/core.clj
+++ test/lolja/test/core.clj
@@ -2,5 +2,30 @@
   (:use [lolja.core])
   (:use [clojure.test]))
 
-(deftest replace-me ;; FIXME: write
-  (is false "No tests have been written."))
+(deftest test-read-command
+  (is (= [:fd 100] (read-command "fd 100")))
+  (is (= [:rt 100] (read-command "rt 100"))))
+
+(deftest test-new-turtle
+  (let [t (new-turtle)]
+    (is (= '(0 0) (:location t)))
+    (is (= 0 (:heading t)))
+    (is (= :pen-down (pen-state t)))))
+
+(deftest test-pen-down
+  (is (= :pen-down (pen-state (pen-down (new-turtle)))))
+  (is (= :pen-down (pen-state (pen-down (pen-up (new-turtle)))))))
+
+(deftest test-pen-erase
+  (is (= :pen-erase (pen-state (pen-erase (new-turtle)))))
+  (is (= :pen-erase (pen-state (pen-erase (pen-down (new-turtle))))))
+  (is (= :pen-erase (pen-state (pen-erase (pen-up (new-turtle)))))))
+  
+(deftest test-pen-up
+  (is (= :pen-up (pen-state (pen-up (new-turtle)))))
+  (is (= :pen-up (pen-state (pen-up (pen-up (new-turtle)))))))
+
+(deftest test-pen-state
+  (is (= :pen-up (pen-state (pen-up (new-turtle)))))
+  (is (= :pen-down (pen-state (pen-down (new-turtle)))))
+  (is (= :pen-erase (pen-state (pen-erase (new-turtle))))))
